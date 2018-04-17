459552fc2f961175e4e3f73302ac2ba3965a8b81
efox711 <38092625+efox711@users.noreply.github.com>
2018-04-17 15:06:04 -0500

    new HW4 project

---
+++ HW4/HW4.X/main.c
@@ -0,0 +1,37 @@
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+
+// DEVCFG0
+#pragma config DEBUG = 0b10 // no debugging
+#pragma config JTAGEN = 0 // no jtag
+#pragma config ICESEL = 0b11 // use PGED1 and PGEC1
+#pragma config PWP = 0b1111111 // no write protect
+#pragma config BWP = 1 // no boot write protect
+#pragma config CP = 1 // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = 0b011 // use primary oscillator with pll
+#pragma config FSOSCEN = 0 // turn off secondary oscillator
+#pragma config IESO = 0 // no switching clocks
+#pragma config POSCMOD = 0b10 // high speed crystal mode
+#pragma config OSCIOFNC = 1 // disable secondary osc
+#pragma config FPBDIV = 0 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = 0b11 // do not enable clock switch
+#pragma config WDTPS = 0 // use slowest wdt
+#pragma config WINDIS = 1 // wdt no window mode
+#pragma config FWDTEN = 0 // wdt disabled
+#pragma config FWDTWINSZ = 0b11 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = 0b001 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = 0b111 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = 0b001 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = 0b001 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = 0 // USB clock on
+
+// DEVCFG3
+#pragma config USERID = 0b0000000000000001 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = 0 // allow multiple reconfigurations
+#pragma config IOL1WAY = 0 // allow multiple reconfigurations
+#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
+#pragma config FVBUSONIO = 1 // USB BUSON controlled by USB module
