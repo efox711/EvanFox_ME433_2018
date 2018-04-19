6370a30214d348e809b73f69dd53d53c154775e9
efox711 <38092625+efox711@users.noreply.github.com>
2018-04-18 20:28:26 -0500

    update

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -50,7 +50,7 @@ unsigned char spi_io(unsigned char o) {
 // initialize spi1
 void spi_init() {
   // declare which pins are set for SPI
-    SS1Rbits.SS1R = 0001;
+  // SS1Rbits.SS1R = 0001;
     RPB2Rbits.RPB2R = 0011;
     SDI1Rbits.SDI1R = 0100;
     
@@ -62,18 +62,13 @@ void spi_init() {
   // we manually control SS1 as a digital output (B3)
   // since the pic is just starting, we know that spi is off. We rely on defaults here
  
-  // setup spi1
+  // setup spi 1
   SPI1CON = 0;              // turn off the spi module and reset it
   SPI1BUF;                  // clear the rx buffer by reading from it
   SPI1BRG = 0x1;            // baud rate to 10 MHz [SPI1BRG = (48000000/(2*desired))-1]
-  SPI4STATbits.SPIROV = 0;  // clear the overflow bit
-  SPI4CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
-  SPI4CONbits.MSTEN = 1;    // master operation
-  SPI4CONbits.ON = 1;       // turn on spi 4
-
-                            // send a ram set status command.
-  CS = 0;                   // enable the ram
-  spi_io(0x01);             // ram write status
-  spi_io(0x41);             // sequential mode (mode = 0b01), hold disabled (hold = 0)
-  CS = 1;                   // finish the command
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;    // master operation
+  SPI1CONbits.ON = 1;       // turn on spi 1
 }
+
